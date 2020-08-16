08d4857ff25fa926aece15b3cfb3f3f25cf9ec12
JK <saikitjk@gmail.com>
2020-08-15 18:31:52 -0700

    fixed undefined errors occured on the readmeTeample.js

--- mdFormat.js
+++ mdFormat.js
@@ -1,26 +1,51 @@
-const mdFormat = data =>{
+const readmeTemplate= data =>{
+    console.log(typeof(data)+data);
+    console.log(data.license);
+    return `
+        # Porject Title
+        # ${data.title}
 
-    return`
-        * Porject Title
-        ${data.title}
+        ![License: ${}]
 
-        * Description
+        __Description__
         ${data.desc}
 
-        * Installation
+        ## table of contents
+        * [Install](#installation)
+
+        * [Usage](#Usage)
+        
+        * [License](#license)
+
+        * [Contributors](#contributors)
+
+        * [Tests](#tests)
+
+        * [Questions](#Additional questions?)
+
+
+        __Installation__
         ${data.install}
 
-        * Usage
+        __Usage__
         ${data.usage}
 
-        * Contributors
+        __License__
+        
+        This project is licensed under ${data.license}
+
+        __Contributors__
         ${data.conGuide}
 
-        * Tests
+        __Tests__
         ${data.testInst}
 
-        * Additional questions?
-        ${data.uEmail}
-        ${data.gUser}
+        __Additional questions__
+
+        If you have any questions about the repo, open an issue or contact me direct at ${data.uEmail}.
+        You can find more of my work at [${data.gUser}](https://github.com/${data.gUser}/).
+
     `
 }
+
+module.exports = readmeTemplate
