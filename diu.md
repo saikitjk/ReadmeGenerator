f6ec38cd3d848ca65981059a43aaacb142e23871
JK <saikitjk@gmail.com>
2020-08-15 19:05:59 -0700

    testing readme format

---
+++ diu.md
@@ -0,0 +1,41 @@
+
+# Porject Title
+# diu
+
+__Description__
+no
+
+## table of contents
+* [Install](#installation)
+
+* [Usage](#Usage)
+        
+* [License](#license)
+
+* [Contributors](#contributors)
+
+* [Tests](#tests)
+
+* [Questions](#Additional questions?)
+
+
+__Installation__
+haha
+
+__Usage__
+yes
+
+__License__
+        
+This project is licensed under MIT
+
+__Contributors__
+ll
+
+__Tests__
+dllm
+
+__Additional questions__
+
+If you have any questions about the repo, open an issue or contact me direct at diu@diu.com.
+You can find more of my work at [jk](https://github.com/jk/).
--- mdFormat.js
+++ mdFormat.js
@@ -2,47 +2,47 @@ const readmeTemplate= data =>{
     console.log(typeof(data)+data);
     console.log(data.license);
     return `
-        # Porject Title
-        # ${data.title}
+# Porject Title
+# ${data.title}
 
-        __Description__
-        ${data.desc}
+__Description__
+${data.desc}
 
-        ## table of contents
-        * [Install](#installation)
+## table of contents
+* [Install](#installation)
 
-        * [Usage](#Usage)
+* [Usage](#Usage)
         
-        * [License](#license)
+* [License](#license)
 
-        * [Contributors](#contributors)
+* [Contributors](#contributors)
 
-        * [Tests](#tests)
+* [Tests](#tests)
 
-        * [Questions](#Additional questions?)
+* [Questions](#Additional questions?)
 
 
-        __Installation__
-        ${data.install}
+__Installation__
+${data.install}
 
-        __Usage__
-        ${data.usage}
+__Usage__
+${data.usage}
 
-        __License__
+__License__
         
-        This project is licensed under ${data.license}
+This project is licensed under ${data.license}
 
-        __Contributors__
-        ${data.conGuide}
+__Contributors__
+${data.conGuide}
 
-        __Tests__
-        ${data.testInst}
+__Tests__
+${data.testInst}
 
-        __Additional questions__
+__Additional questions__
 
-        If you have any questions about the repo, open an issue or contact me direct at ${data.uEmail}.
-        You can find more of my work at [${data.gUser}](https://github.com/${data.gUser}/).
-    `
+If you have any questions about the repo, open an issue or contact me direct at ${data.uEmail}.
+You can find more of my work at [${data.gUser}](https://github.com/${data.gUser}/).
+`
 }
 
 module.exports = readmeTemplate
